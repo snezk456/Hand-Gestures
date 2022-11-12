2a87bfd807abdb3cca6bf04d0872d12dafcc2e33
snezk456 <86143353+snezk456@users.noreply.github.com>
2022-11-12 15:12:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,85 @@
-Prediction_1 = "";
-Prediction_2 = "";
 Webcam.set({
     width:350,
     height:300,
-    image_format:'png',
+    image_format : 'png',
     png_quality:90
-});
-camera = document.getElementById("camera");
-Webcam.attach('#camera');
+  });
 
-function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
-console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oSVVO9TO8/model.json', modelLoaded);
-function modelLoaded(){
-    console.log('Model is Loaded');
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+
+    gesture = results[0].label;
+    
+    toSpeak = "";
+    if(gesture == "amazing")
+    {
+      toSpeak = "This is looking amazing";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;";
+    }
+    else if(gesture == "best")
+    {
+      toSpeak = "All the best";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
+    }
+    else if(gesture == "victory")
+    {
+      toSpeak = "That was the marvelous victory";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
+    }
+    else if(gesture == "raised fist")
+    {
+        toSpeak = "It conveys various ideas of empowerment, political protest and resistance, and civil disobedience."
+        document.getElementById("result_object_gesture_icon").innerHTML = "&#9994;";
+    }
+    else if(gesture == "rock and roll")
+    {
+        toSpeak = "It is used to convey a kind of 'rock on!' enthusiasm";
+        document.getElementById("results_object_gesture_icon").innerHTML = "&#129304;";
+    }
+
+    speak();
+  }
 }
+
+
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data_1="The first prediction is " + Prediction_1;
-    speak_data_2 = "and The second prediction is " + Prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+
+    speak_data = toSpeak;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
     synth.speak(utterThis);
+
 }
